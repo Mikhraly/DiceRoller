d835435dfa0fb0619fbe6e34dfcfdeb1be76be9b
Mikhraly <mikhraly@gmail.com>
2021-12-12 01:51:55 +0300

    Optimization my code

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -26,25 +25,29 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val diceImage: ImageView = findViewById(R.id.image_view)
-        when (diceRoll) {
-            1 -> diceImage.setImageResource(R.drawable.dice_1)
-            2 -> diceImage.setImageResource(R.drawable.dice_2)
-            3 -> diceImage.setImageResource(R.drawable.dice_3)
-            4 -> diceImage.setImageResource(R.drawable.dice_4)
-            5 -> diceImage.setImageResource(R.drawable.dice_5)
-            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
 
         val diceRoll2 = dice.roll()
         val diceImage2: ImageView = findViewById(R.id.image_view_2)
-        when (diceRoll2) {
-            1 -> diceImage2.setImageResource(R.drawable.dice_1)
-            2 -> diceImage2.setImageResource(R.drawable.dice_2)
-            3 -> diceImage2.setImageResource(R.drawable.dice_3)
-            4 -> diceImage2.setImageResource(R.drawable.dice_4)
-            5 -> diceImage2.setImageResource(R.drawable.dice_5)
-            6 -> diceImage2.setImageResource(R.drawable.dice_6)
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        diceImage2.setImageResource(drawableResource2)
+        diceImage2.contentDescription = diceRoll2.toString()
     }
 }
 
